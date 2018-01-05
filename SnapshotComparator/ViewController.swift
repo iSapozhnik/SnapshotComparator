a0e4c29c19d3b794429798100fda061014f8adce
Ivan Sapozhnik <ivan.sapozhnik@sixt.com>
2018-01-05 11:19:49 +0100

    Initial Commit

---
+++ SnapshotComparator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SnapshotComparator
+//
+//  Created by Ivan Sapozhnik on 05.01.18.
+//  Copyright © 2018 Ivan Sapozhnik. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
